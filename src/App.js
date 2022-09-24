fc900e390a3474e86f7a46920a7ac28a98abbcd5
robiulislam2520 <robiul215565@gmail.com>
2022-09-24 15:59:51 +0600

    local storage done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Cart from "./components/Cart/Cart";
 import Cosmetics from "./components/Cosmetics/Cosmetics";
 
 function App() {
   return (
     <div className="App">
       <h2>Hello world</h2>
+      <Cart/>
       <Cosmetics/>
     </div>
   );
--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { add, multiply } from '../../utils/calculate';
+import React, { useEffect, useState } from 'react';
+import Cosmetic from './Cosmetic';
 
 export default function Cosmetics() {
-    const num1 = 55;
-    const num2 = 125;
-    const total = add(num1,num2);
+    const [cosmetics,setCosmetics] = useState([]);
+
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setCosmetics(data))
+        .catch(err => console.log(err.message))
+    }, [])
 
   return (
     <>
         <h1>Calculate all things</h1>
-        <h3>Total add number : {total}</h3>
-        <h3>Multiply all number : {multiply(5,4)}</h3>
+        <div className='grid'>
+        {
+            cosmetics.map(cosmetic => <Cosmetic key={cosmetic.id} cosmetic={cosmetic}/>)
+        }
+        </div>
     </>
   )
 }
