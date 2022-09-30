134b53b414f9fdb79d8ae1a7825cf48f33c69ded
robiulislam2520 <robiul215565@gmail.com>
2022-10-01 00:54:12 +0600

    tailwind css

---
+++ tailwind-css/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline hover:text-4xl duration-500 ease-in hover:bg-lime-600 hover:p-6 hover:text-white cursor-pointer">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
