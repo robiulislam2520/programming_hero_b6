af2628679e1bf350f4f99093a3c9ae7a3cf7af7b
robiulislam2520 <robiul215565@gmail.com>
2022-09-30 23:29:14 +0600

    learn React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import NavbarComponent from "./components/Navbar/Navbar";
+import ToastComponent from "./components/Toast/Toast";
+
 function App() {
   return (
     <div className="App">
       <h2>Hello world</h2>
+      <NavbarComponent/>
+      <ToastComponent/>
     </div>
   );
 }
