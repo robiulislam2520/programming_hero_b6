9d98c82cc124248854d83b3a7136afe497a72556
robiulislam2520 <robiul215565@gmail.com>
2022-10-07 08:27:12 +0600

    Remove item

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import './order.css';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { removeFromDb } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import "./order.css";
 
 const Orders = () => {
-  const {products, initialCart} = useLoaderData();
-  console.log(initialCart)
-  return (
-    <div className='order-products container'>
-      {
-        initialCart.map(pd => {
-          return <div key={pd.id} className>
-          <div className="flex">
-            <div>
-              <img src={pd.img} alt={pd.name} />
-            </div>
-            <div>
-              <h3>Qty: {pd.quantity}</h3>
-            </div>
-          </div>
-        </div>
-        })
-      }
-    </div>
-  )
-}
+  const { initialCart } = useLoaderData();
+  const [cart, setCart] = useState(initialCart);
 
-export default Orders
+  const handleDelete = (id) => {
+    const deletedCart = initialCart.filter(product => product.id !== id);
+    setCart(deletedCart);
+    removeFromDb(id)
+  }
+  // console.log(initialCart)
+  return (
+    <div className="container">
+      <div className="orders-products">
+        <div className="products">
+          {cart.map((pd) => {
+            return (
+              <div key={pd.id}>
+                <div className="flex">
+                  <div>
+                    <img src={pd.img} alt={pd.name} />
+                  </div>
+                  <div>
+                    <h3>Qty: {pd.quantity}</h3>
+                    <p>Price: ${pd.price * pd.quantity}</p>
+                    <button onClick={() => handleDelete(pd.id)}>Delete</button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="cart-container">
+          <Cart cart={cart} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
