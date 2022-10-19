edcd68705d6aafb3ec52d644f9ce14585910ab34
robiulislam2520 <robiul215565@gmail.com>
2022-10-19 08:41:09 +0600

    Firebase and react-bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Register from "./components/Register";
+
 function App() {
   return (
-    <div className="App">
-      <h2>Hello world</h2>
+    <div className="container w-75 mx-auto mt-5">
+      <Register/>
     </div>
   );
 }
---
+++ src/components/Register.jsx
@@ -0,0 +1,37 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+function RegisterForm() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(email, password);
+  };
+  return (
+    <div className="py-5">
+      <h2 className="text-primary text-bold">Please Register Now !!!</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" name="email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default RegisterForm;
