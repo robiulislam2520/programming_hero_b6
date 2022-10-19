cb22ffaabe5da1e84e3ebc9926189620b9996e41
robiulislam2520 <robiul215565@gmail.com>
2022-10-20 00:03:28 +0600

    Usecontxt update

--- src/contexts/UseContext.js
+++ src/contexts/UseContext.js
@@ -1,6 +1,6 @@
 import React, { createContext } from 'react';
 
-const AuthContext = createContext()
+export const AuthContext = createContext();
 
 const UseContext = ({children}) => {
     const user = {
