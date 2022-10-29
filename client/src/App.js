71c95ff3a3fac8f5d99440b22bcd554e92bbb93e
robiulislam2520 <robiul215565@gmail.com>
2022-10-30 00:29:10 +0600

    Server query added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,57 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  // get all users
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => {
+      setUsers(data)
+    })
+  }, [users])
+
+  // handle submit function
+  const handleSubmit = e =>{
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const name = form.name.value;
+
+    const newUser = {
+      name,
+      email
+    }
+
+    // post users data
+    fetch('http://localhost:5000/user', {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(newUser)
+    })
+
+   
+
+    form.reset();
+
+  }
+
+  console.log(users)
+
   return (
     <div className="App">
-      <h2>Hello world</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="" required/> 
+        <br /><br />
+        <input type="email" name="email" id="" required/> <br />
+        <input type="submit" value="Submit" />
+      </form>
+      {
+        users.map(user => <h2 key={user?.id}>{user.id}. {user?.name} - {user?.email}</h2>)
+      }
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -8,13 +8,44 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(express.json());
+
+// users
+const users = [
+    {id: 1, name: 'Robiul Islam', email: 'robiul@gmail.com'},
+    {id: 2, name: 'Mizanr Rahman', email: 'mizan@gmail.com'},
+    {id: 3, name: 'Limon hasan', email: 'limon@gmail.com'},
+]
+
+
 
 // Routes
 app.get('/health', (req, res) =>{
     res.json({"message" : "Server health is good"})
 })
+app.get('/', (req, res) =>{
+    res.send("<h1>Node js server is running</h1>")
+})
+app.get('/users', (req, res) =>{
+    if(req.query.name){
+        const search = req.query.name;
+        const filtered = users.filter(usr => usr.name.toLocaleLowerCase().indexOf(search) >= 0);
 
-// App listen on http://localhost:500
+        res.send(filtered);
+    } else{
+        res.json(users)
+    }
+})
+
+app.post('/user', (req, res) =>{
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+
+    res.json(users);
+})
+
+// App listen on http://localhost:5000
 app.listen(port, ()=>{
     console.log("server is running on port" , port);
 })
