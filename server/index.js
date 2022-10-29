a2023767b2479e56f5dcdd603d110138fb418eb8
robiulislam2520 <robiul215565@gmail.com>
2022-10-30 02:52:03 +0600

    Solve some issue

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Create a app
 const app = express();
@@ -10,6 +11,32 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json())
+
+
+// Database connection
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster1.ftnnc4j.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    const userCollection = client.db("nodeMongoCrud").collection("users");
+
+    const user = {
+      name: "Nasim Biswas",
+      email: "nasim@gmail.com"
+    }
+
+    const result = await userCollection.insertOne(user);
+
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(err => console.log(err.message));
+
+
+
 
 // Routes
 app.get("/health", (req, res) => {
