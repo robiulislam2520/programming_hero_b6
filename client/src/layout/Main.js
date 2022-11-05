1b1b2e3b2d8ddaa570969b7421956b2e5d004305
Robiul Islam <robiul215565@gmail.com>
2022-11-06 02:18:32 +0600

    Toast added

--- client/src/layout/Main.js
+++ client/src/layout/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
+import {Toaster} from 'react-hot-toast';
 import Header from '../components/Header';
 
 const Main = () => {
@@ -9,6 +10,7 @@ const Main = () => {
         <Header/>
         <div className='container mx-auto'>
           <Outlet/>
+          <Toaster position="top-center"/>
         </div>
         <Footer/>
     </>
