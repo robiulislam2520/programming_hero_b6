cacfe8e4c66787300b4b9ec3378badae4febb399
Robiul Islam <robiul215565@gmail.com>
2022-11-12 00:13:58 +0600

    Linting setup

---
+++ client/src/pages/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='container mx-auto py-12'>
+        <h1 className="text-2xl font-semibold text-center">
+          404 Page NotFound
+        </h1>
+    </div>
+  )
+}
+
+export default NotFound
